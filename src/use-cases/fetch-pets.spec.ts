b951604d6e4fbea37684c12a297cf13fed6b8ddf
Enilton Rodrigues de Andrade <erodri@Eniltons-MacBook-Pro-2.local>
2023-04-21 08:03:15 -0500

    test: adds test for fetch filtered pets use case

--- src/use-cases/fetch-pets.spec.ts
+++ src/use-cases/fetch-pets.spec.ts
@@ -78,4 +78,61 @@ describe('Fetch Pets by City Use Case', () => {
       }),
     ).rejects.toBeInstanceOf(ResourceNotProvidedError)
   })
+
+  it('should be able to fetch pets by city and Independence level filter', async () => {
+    petsRepository.orgs.push({
+      address_city: 'Sorocaba',
+      address_number: '2',
+      address_state: 'São Paulo',
+      address_street: 'Rua do Meio',
+      email: 'email@example.com',
+      id: 'ORG01',
+      created_at: new Date(),
+      name: 'ORG 1',
+      password_hash: 'password',
+      phone: '00000000',
+    })
+    petsRepository.orgs.push({
+      address_city: 'Sorocaba',
+      address_number: '2',
+      address_state: 'São Paulo',
+      address_street: 'Rua do Meio',
+      email: 'email@example.com',
+      id: 'ORG04',
+      created_at: new Date(),
+      name: 'ORG 4',
+      password_hash: 'password',
+      phone: '00000000',
+    })
+
+    await petsRepository.create({
+      age: 7,
+      description: 'clean dog',
+      energy_level: 'HIGH',
+      environment_type: 'BIG',
+      independence_level: 'LOW',
+      name: 'Scooby',
+      org_id: 'ORG01',
+      size: 'BIG',
+    })
+
+    await petsRepository.create({
+      age: 7,
+      description: 'clean dog',
+      energy_level: 'HIGH',
+      environment_type: 'BIG',
+      independence_level: 'HIGH',
+      name: 'Scooby',
+      org_id: 'ORG04',
+      size: 'BIG',
+    })
+    const { pets } = await sut.execute({
+      city: 'Sorocaba',
+      environmentType: 'BIG',
+      independenceLevel: 'LOW',
+    })
+
+    expect(pets).toHaveLength(1)
+    expect(pets).toEqual([expect.objectContaining({ org_id: 'ORG01' })])
+  })
 })
